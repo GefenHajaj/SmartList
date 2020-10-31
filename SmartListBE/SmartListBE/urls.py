ed80ee2d3fb1318c04889b7233380a9434145f14
GefenHajaj <gefenhajaj@gmail.com>
2020-10-31 16:10:52 +0200

    made some changes to server of prod

--- SmartListBE/SmartListBE/urls.py
+++ SmartListBE/SmartListBE/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('smartlist/', include('SmartListApp.urls')),
-    path('admin/', admin.site.urls),
+    path('smartlistadmin/', admin.site.urls),
 ]
