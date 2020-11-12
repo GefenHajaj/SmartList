1e37dc79e6af41d63e72eae1392d8fe90be58dd9
GefenHajaj <gefenhajaj@gmail.com>
2020-11-12 19:14:54 +0200

    changed to real app id and banner unit id - for ads

--- smart_list_app/lib/ad_manager.dart
+++ smart_list_app/lib/ad_manager.dart
@@ -17,8 +17,8 @@ class AdManager {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
       // Test Only!!!
-      return "ca-app-pub-3940256099942544/6300978111";
-      // return "ca-app-pub-7581133893794696/3921367906";
+      // return "ca-app-pub-3940256099942544/6300978111";
+      return "ca-app-pub-7581133893794696/3921367906";
     }
     // else if (Platform.isIOS) {
     //   return "<YOUR_IOS_BANNER_AD_UNIT_ID>";
@@ -27,28 +27,4 @@ class AdManager {
       throw new UnsupportedError("Unsupported platform");
     }
   }
-
-  static String get interstitialAdUnitId {
-    if (Platform.isAndroid) {
-      // Test only!!!
-      return "ca-app-pub-3940256099942544/1033173712";
-      // return "ca-app-pub-7581133893794696/6355959554";
-    }
-    // else if (Platform.isIOS) {
-    //   return "<YOUR_IOS_INTERSTITIAL_AD_UNIT_ID>";
-    // }
-    else {
-      throw new UnsupportedError("Unsupported platform");
-    }
-  }
-
-  // static String get rewardedAdUnitId {
-  //   if (Platform.isAndroid) {
-  //     return "<YOUR_ANDROID_REWARDED_AD_UNIT_ID>";
-  //   } else if (Platform.isIOS) {
-  //     return "<YOUR_IOS_REWARDED_AD_UNIT_ID>";
-  //   } else {
-  //     throw new UnsupportedError("Unsupported platform");
-  //   }
-  // }
 }
