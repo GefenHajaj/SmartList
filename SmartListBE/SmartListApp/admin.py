fdb56d67595a537d5cce330c84585e8f63d731f3
GefenHajaj <gefenhajaj@gmail.com>
2021-06-27 19:48:23 +0300

    Added created_time field of ListObject in admin page

--- SmartListBE/SmartListApp/admin.py
+++ SmartListBE/SmartListApp/admin.py
@@ -11,9 +11,13 @@ class ShoppingListAdmin(admin.ModelAdmin):
     readonly_fields = ('last_changed',)
 
 
+class ListObjectAdmin(admin.ModelAdmin):
+    readonly_fields = ('creation_time',)
+
+
 admin.site.register(User, UserAdmin)
 admin.site.register(ShoppingList, ShoppingListAdmin)
 admin.site.register(Product)
-admin.site.register(ListObject)
+admin.site.register(ListObject, ListObjectAdmin)
 
 
